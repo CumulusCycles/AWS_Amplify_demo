294882deba01bedd2aabbb4028fbc6e1ae702ac3
CumulusCycles <cumuluscycles@gmail.com>
2022-11-10 19:41:45 -0500

    Added common components, Auth and Routing

---
+++ contacts-app/src/components/common/SiteFooter.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+function SiteFooter() {
+    return (
+        <footer className="py-5 my-5">
+            <Container className="px-4">
+                <p className="text-end">&copy; 2022</p>
+            </Container>
+        </footer>
+    )
+}
+
+export default SiteFooter;
